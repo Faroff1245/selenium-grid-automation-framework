e5a5b0df7fec5e8c4ae36e4f015354599eb041ea
harshith-saga012sig <premiumcave@gmail.com>
2026-04-15 23:20:51 +0530

    Initial commit - Selenium Grid + TestNG parallel execution

---
+++ src/main/java/com/grid/selenium_grid_project/App.java
@@ -0,0 +1,13 @@
+package com.grid.selenium_grid_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
